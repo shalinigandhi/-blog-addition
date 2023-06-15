e5499425cda5b0ce1466f6306e37514ea3c697eb
Shalini Gandhi <shalinigandhi2@gmail.com>
2023-06-15 18:36:23 -0400

    UI for tab navigation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { categories } from '../../data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import './home.scss';
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    const [active, setActive] = React.useState(categories[0]);
+    return (
+        <div className="home-page-container">
+            <div className="home-page-wrapper">
+                <div className="container">
+                    <h1 className="heading">Categories</h1>
+                    <div className="navigation-container">
+                        <ul className="tabs-navigation-container">
+                            {
+                                categories.map((category, index) => (
+                                    <li className="tabs-navigation-list" key={index} onClick={() => setActive(category)}>
+                                        <a className={"tabs-navigation-link" + (active ===  category ? " active" : '')}>{category}</a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                        <button className="primary-btn">
+                            <span className="btn-text">Add Topic </span>
+                            <FontAwesomeIcon icon={faAngleRight} style={{color: "#ffffff"}}/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Home
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,1 @@
+export const categories = ['All', 'Custom', 'ICP', 'Mission', 'Product'];
